5efa2179d6b71424b54a5e2cfe993c53bced7228
Rushi <hrushikeshborakhede1089@gmail.com>
2022-02-15 09:55:24 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,27 @@
+from Login import Login
+from form import LoginForm
+from PyQt5 import QtCore, QtGui, QtWidgets
+import sys
+
+
+app = QtWidgets.QApplication(sys.argv)
+
+ui = Login()
+lf = LoginForm()
+
+def changeLabel():
+    ui.changeWindow.clicked.connect(changeFrame)
+    ui.show()
+    lf.hide()
+
+def changeFrame():
+    print('You have clicked change form')
+    lf.addButton.clicked.connect(changeLabel)
+    lf.show()
+    ui.hide()
+
+ui.changeWindow.clicked.connect(changeFrame)
+ui.show()
+
+
+sys.exit(app.exec_())
