e92c616df3d9808c8e756cc0f08122b58548ccd9
Rushi <hrushikeshborakhede1089@gmail.com>
2022-02-22 21:56:07 +0530

    Update Login.py

--- Login.py
+++ Login.py
@@ -1,14 +1,15 @@
-from frontend.esha import Ui_Dialog
+from frontend.LoginWindow import Ui_LoginWindow
 from PyQt5 import QtCore, QtGui, QtWidgets
 from form import LoginForm
 import sys
 
-class Login(QtWidgets.QMainWindow,Ui_Dialog):
+class Login(QtWidgets.QMainWindow,Ui_LoginWindow):
     def __init__(self) -> None:
         super().__init__()
         print('I am login imported from Login')
         self.setupUi(self)
-        self.changeWindow.clicked.connect(self.changeFrame)
+        self.loginButton.clicked.connect(self.changeFrame)
+        # self.changeWindow.clicked.connect(self.changeFrame)
 
     def changeFrame(self):
         print('hello world')
