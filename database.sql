f127bcb0fe67656022d4bd00726f97fd80cecdf1
Rushi <hrushikeshborakhede1089@gmail.com>
2022-03-08 11:27:21 +0530

    Create database.sql

---
+++ database.sql
@@ -0,0 +1,26 @@
+CREATE TABLE patient (
+    FirstName VARCHAR(50),
+    LastName VARCHAR(50),
+    Mobile BIGINT,
+    Email VARCHAR(100),
+    Address VARCHAR(300),
+    Password VARCHAR(15)
+);
+
+CREATE TABLE doctor (
+    FirstName VARCHAR(50),
+    LastName VARCHAR(50),
+    Mobile BIGINT,
+    Email VARCHAR(100),
+    Address VARCHAR(300),
+    Password VARCHAR(15),
+    College VARCHAR(75),
+    Education VARCHAR(10),
+    Specialization VARCHAR(50)
+);
+
+ALTER TABLE patient 
+    ADD CONSTRAINT Pk_Patient PRIMARY KEY (Email);
+
+ALTER TABLE doctor
+    ADD CONSTRAINT Pk_Doctor PRIMARY KEY (Email);
