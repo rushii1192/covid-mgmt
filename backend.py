c0a2606f882a04f2c3b4612ba3265d41c3ce9418
Rushi <hrushikeshborakhede1089@gmail.com>
2022-03-08 11:27:17 +0530

    Update backend.py

--- backend.py
+++ backend.py
@@ -1,3 +1,22 @@
 #Database Connection
+import mysql.connector as mysql
+import smtplib
+
+#class for data connectivity with MySql
+class DatabaseConnection(object):
+    def __new__(cls, *args, **kwargs):
+        conn = mysql.connect(
+            host='db4free.net',
+            database='covid_mgmt',
+            user='sql6468008',
+            password='eqHRn7SbqA'
+        )
+        print("connected")
+        print(type(conn))
+        return conn
+
+    def __del__(self):
+        self.conn.close()
 
 #Exception Handling
+dc = DatabaseConnection()
