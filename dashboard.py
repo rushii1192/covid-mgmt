af6caf4ccf2efc324708bba3f738d25e73e79938
Rushi <hrushikeshborakhede1089@gmail.com>
2022-03-21 22:01:38 +0530

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,6 +1,7 @@
 from PyQt5 import QtWidgets
 from frontend.custDashboardWindow import Ui_CustomerDashboardWindow
 from frontend.doctorDashboardWindow import Ui_DoctorDashboard
+from frontend.maindashboard import Ui_MainDashboard
 
 class CustomerDashboard(QtWidgets.QMainWindow,Ui_CustomerDashboardWindow):
     def __init__(self) -> None:
@@ -11,5 +12,9 @@ class CustomerDashboard(QtWidgets.QMainWindow,Ui_CustomerDashboardWindow):
 class DoctorDashboard(QtWidgets.QMainWindow,Ui_DoctorDashboard):
     def __init__(self) -> None:
         super().__init__()
-        print('I am Regsiter imported from Login')
+        self.setupUi(self)
+
+class WelcomePage(QtWidgets.QMainWindow,Ui_MainDashboard):
+    def __init__(self) -> None:
+        super().__init__()
         self.setupUi(self)
