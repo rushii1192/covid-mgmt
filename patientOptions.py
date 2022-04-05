0bd9a63c779602b8bbfcb5ee0ee9c5ebc9796c43
Rushi <hrushikeshborakhede1089@gmail.com>
2022-04-06 01:00:34 +0530

    Update patientOptions.py

--- patientOptions.py
+++ patientOptions.py
@@ -5,6 +5,8 @@ from PyQt5 import QtWidgets,QtGui
 from backend import DatabaseConnection
 class PatientHistory(QtWidgets.QMainWindow,Ui_PatientHistory):
     __patientId = ""
+    __dc = DatabaseConnection()
+    __cursor = __dc.cursor()
     def __init__(self,patientId) -> None:
         super().__init__()
         self.setupUi(self)
@@ -19,20 +21,32 @@ class PatientHistory(QtWidgets.QMainWindow,Ui_PatientHistory):
         self.historyTable.setColumnWidth(1, 250)
         self.historyTable.setColumnWidth(2, 250)
 
-        self.historyTable.setItem(0, 0, QtWidgets.QTableWidgetItem("Hello"))
-        self.historyTable.setItem(0, 1, QtWidgets.QTableWidgetItem("Hello"))
-        self.historyTable.setItem(0, 2, QtWidgets.QTableWidgetItem("Hello"))
+        self.__cursor.execute(f"SELECT * FROM appointment WHERE PatientId = '{self.__patientId}';")
+        result = self.__cursor.fetchall()
+        for count, row in enumerate(result):
+            self.historyTable.setItem(count, 0, QtWidgets.QTableWidgetItem(f'{row[4]}'))
+            self.historyTable.setItem(count, 1, QtWidgets.QTableWidgetItem(f'{row[-1]}'))
+            self.historyTable.setItem(count, 2, QtWidgets.QTableWidgetItem(f'{row[1]}'))
 
 class PatientAppointment(QtWidgets.QMainWindow,Ui_PatientAppointment):
     __doctorId = ""
     __patientId = ""
+    __dc = DatabaseConnection()
+    __cursor = __dc.cursor()
     def __init__(self) -> None:
         super().__init__()
         self.setupUi(self)
         self.requestButton.clicked.connect(self.requestAppointment)
 
     def requestAppointment(self):
-        print(f"INSERT INTO appointment VALUES('meet_id','{self.prefferedDatetTime.dateTime().toPyDateTime()}','45hrs','{self.__patientId} {self.healthIssue.text()}','{self.__doctorId}','{self.__patientId}','{self.healthIssue.text()}')")
+        self.__cursor.execute(f"INSERT INTO appointment VALUES('meet_id','{self.prefferedDatetTime.dateTime().toPyDateTime()}','45hrs','{self.__patientId} {self.healthIssue.text()}','{self.__doctorId}','{self.__patientId}','{self.healthIssue.text()}')")
+        self.__dc.commit()
+        msg = QtWidgets.QMessageBox(self)
+        msg.setWindowTitle('Successfull')
+        msg.setText(f'Your appointment has been sent to doctor.')
+        msg.setStyleSheet('color:black')
+        msg.setStandardButtons(QtWidgets.QMessageBox.Ok)
+        msg.exec_()
 
     def setIds(self,doctorid,patientId):
         self.__doctorId = doctorid
