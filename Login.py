85ee57ab785a07b0abe2fa24bc3f7caa78c58490
Rushi <hrushikeshborakhede1089@gmail.com>
2022-04-05 19:56:24 +0530

    Update Login.py

--- Login.py
+++ Login.py
@@ -15,7 +15,7 @@ class Login(QtWidgets.QMainWindow,Ui_LoginWindow):
         msg = QtWidgets.QMessageBox(self)
         msg.setWindowTitle('Login Error')
         msg.setText('Hey You have entered wrong credintials\nPlease check your username and password')
-        msg.setStyleSheet('color:white')
+        msg.setStyleSheet('color:black')
         msg.setStandardButtons(QtWidgets.QMessageBox.Ok)
         msg.exec_()
         # ui.setupUi(Dialog)
@@ -24,12 +24,14 @@ class Login(QtWidgets.QMainWindow,Ui_LoginWindow):
         dc = DatabaseConnection()
         cursor = dc.cursor()
         cursor.execute(f"SELECT * FROM patient WHERE Email = '{self.usrname.text()}' and Password = '{self.password.text()}'")
-        if cursor.fetchone():
-            return 1
+        result = cursor.fetchone()
+        if result:
+            return (1,result[3])
         else:
             cursor.execute(f"SELECT * FROM doctor WHERE Email = '{self.usrname.text()}' and Password = '{self.password.text()}'")
-            if cursor.fetchone():
-                return 2
+            result = cursor.fetchone()
+            if result:
+                return (2,result[3])
             return 0
 
     def setLogin(self):
